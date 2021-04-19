4bf121eaf307209ee1713bcf5f598e2c5cc7a139
Paul A. Llamoja Sarmiento <76861587+Paul-Llamoja-Sarmiento@users.noreply.github.com>
2021-04-19 15:35:00 -0500

    Create main.cpp

---
+++ MyString/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include "MyString.h"
+#include <vector>
+
+
+int main() 
+{
+	
+	// Copy and move assignment
+
+	/*MyString a {"Hello"};              
+	MyString b;                             
+	b = a;      
+
+	a.display();
+	b.display();
+
+	b = "This is a test";    
+	b.display();
+
+	MyString empty;                      
+   	MyString larry("Larry");          
+    	MyString stooge {larry};             
+    	MyString stooges;   
+
+	empty.display(); 
+
+	empty = stooge;     
+	stooges = "This is another test.";          
+    
+    	empty.display();             
+    	larry.display();                      
+    	stooge.display();       
+	stooges.display();	*/  
+
+
+	
+	//Test with vector and move constructor
+	/*std::vector<MyString> stooges_vec;
+    	stooges_vec.push_back("Larry");        	// Move constructor
+    	stooges_vec.push_back("Moe");		// Move constructor
+    	stooges_vec.push_back("Curly");       	// Move constructor     
+
+	std::cout << "=== Loop 1 ==================" << std::endl;
+    	for (const MyString &s: stooges_vec) 
+       		s.display();
+        
+    	std::cout << "=== Loop 2 ==================" << std::endl;
+    	for (MyString &s: stooges_vec)
+        	s = "Changed";           	// move assignment
+        
+   	std::cout << "=== Loop 3 ==================" << std::endl;
+    	for (const MyString &s: stooges_vec) 
+        	s.display();      */       
+
+	// Test with negation operator
+	/*MyString a{"HoLa a ToDoS"};
+	a.display();
+	MyString b;
+	b = -a;
+	b.display();     */
+
+	// Test with pre and post operators
+	/*MyString a{"This is a test"};
+	a.display();
+	MyString b{"This is another test"};
+	b.display();
+	MyString c;
+	a++;
+	a.display();
+	++b;
+	b.display();*/
+
+	//Testing equality and inequality
+	/*MyString a{"Hello"};
+	MyString b{"Bye"};
+	MyString c{a};
+
+	std::cout << boolalpha << std::endl;
+	std::cout << (a == b) << std::endl;
+	std::cout << (a == c) << std::endl;*/
+
+	//Testing with concatenate operators
+	/*MyString first("Hola ");
+	MyString second("a todos ");
+	first = first + second + "los visitantes";
+	first.display();*/
+
+	// Testing compare operators
+	/*MyString x("Small string");
+	MyString y("This is a larger string");
+
+	std::cout << std::boolalpha << std::endl;
+	std::cout << (x < y) << std::endl;
+	std::cout << (x > y) << std::endl;
+	std::cout << (x > y) << std::endl;
+	std::cout << (x < y) << std::endl;*/
+
+	return 0;  
+}
